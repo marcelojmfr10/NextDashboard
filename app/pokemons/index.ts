1726386340657c2252adde502a14fad752643a10
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-06-17 20:03:44 -0600

    Rutas dinámicas, mostrar página de pokemon y notfound page

--- app/pokemons/index.ts
+++ app/pokemons/index.ts
@@ -1,3 +1,4 @@
 export type { PokemonsResponse } from "./interfaces/pokemons-response";
 export type { SimplePokemon } from "./interfaces/simple-pokemon";
+export type { Pokemon } from "./interfaces/pokemon";
 export * from "./components/PokemonGrid";
